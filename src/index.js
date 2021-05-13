f5e71b666c769aa15aa84021f118b63fab03d17a
RVadim <roibu.vadim@gmail.com>
2021-05-13 18:44:32 +0300

    removed React.strictMode

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import '@fontsource/roboto';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
